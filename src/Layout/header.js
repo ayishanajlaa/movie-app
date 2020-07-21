033a9dd2bc52a8bed8da4191c20ca3361e12eba3
ayishanajlaa <ayishanajla.alaka@hcl.com>
2020-07-21 21:45:13 +0530

    basic-setup

---
+++ src/Layout/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className="App">
+     <nav class="navbar navbar-dark fixed-top bg-custom-2">
+  <div class="container">
+    <a class="navbar-brand" href="#a">JustWatch</a>
+  </div>
+</nav>
+    </div>
+  );
+}
+
+export default Header;
